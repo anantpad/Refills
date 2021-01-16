9add4a6261dd167bce7c917a972fba55c5de274c
anantpad <37480065+anantpad@users.noreply.github.com>
2021-01-16 14:16:04 -0800

    Add files via upload

---
+++ connectSQL.py
@@ -0,0 +1,20 @@
+import pyodbc
+import os
+import dotenv
+
+
+
+conn_str = (
+    'Trusted_Connection=No;'
+    'driver={SQL Server};'
+    'server=DEMOCPS00362001.englab.athenahealth.com;'
+    'database=demodb;'
+    'uid=sa;'
+    'pwd=GEps2006;'
+)
+cnxn = pyodbc.connect(conn_str)
+
+cursor = cnxn.cursor()
+
+for row in cursor.execute('select DISTINCT first,last from PatientProfile'):
+    print(row.first)
